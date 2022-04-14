18822ec37fe0e8eb69385eb99bbda27665b839b9
SHASHI KUMAR <SHASHI KUMAR@DESKTOP-JIJPL74>
2022-04-14 17:44:47 +0530

    change commit

--- src/test/java/com/jenkins/Class1Test.java
+++ src/test/java/com/jenkins/Class1Test.java
@@ -6,6 +6,11 @@ public class Class1Test {
 
 @Test
 public void demo1() {
+	
+	String browser = System.getProperty("BROWSER");
+    String url = System.getProperty("URL");
+	
+	System.out.println(browser+"======"+url);
 	System.out.println("hello junkins");
 }
 	
