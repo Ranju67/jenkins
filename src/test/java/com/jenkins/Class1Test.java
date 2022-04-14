9b8a8fe18916e8b0a60e200ff49c332394b41334
SHASHI KUMAR <SHASHI KUMAR@DESKTOP-JIJPL74>
2022-04-14 13:19:06 +0530

    first commit

---
+++ src/test/java/com/jenkins/Class1Test.java
@@ -0,0 +1,12 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class1Test {
+
+@Test
+public void demo1() {
+	System.out.println("hello junkins");
+}
+	
+}
---
+++ src/test/java/com/jenkins/Class2Test.java
@@ -0,0 +1,11 @@
+package com.jenkins;
+
+import org.testng.annotations.Test;
+
+public class Class2Test {
+
+	@Test
+	public void demo2() {
+		System.out.println("bye junkins");
+	}
+}
